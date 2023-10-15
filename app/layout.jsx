b8497a13806edfdd29576abc5c8ff1c8eceb3119
vileroze <50352881+vileroze@users.noreply.github.com>
2023-10-15 23:39:41 +0545

    Fixed google verification metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,30 @@
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 import "@styles/globals.css";
+import Head from "next/head";
 
 export const metadata = {
   title: "Thoughtstopia",
   description: "For the minds that never rest, share and discover thoughts",
   icons: {
-    icon: '/assets/images/logo_10.png',
+    icon: "/assets/images/logo_10.png",
   },
-  googleSiteVerification: "uudaC6vPPLY39oWpOLoCd4T_wqOUHHgrmsrfbWJ1gN4",
+  verification: {
+    google: "uudaC6vPPLY39oWpOLoCd4T_wqOUHHgrmsrfbWJ1gN4",
+  },
 };
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
+      <Head>
+        {metadata.verification?.google && (
+          <meta
+            name="google-site-verification"
+            content={metadata.verification.google}
+          />
+        )}
+      </Head>
       <body>
         <Provider>
           <div className="main">
